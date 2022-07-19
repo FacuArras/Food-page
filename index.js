7fb0900ca90b392040459d0919b4175e3331d3cd
facundo <facundoarrascaetacba@gmail.com>
2022-07-19 12:02:02 -0300

    loader gif added

--- index.js
+++ index.js
@@ -57,3 +57,13 @@ var swiper = new Swiper(".menuSwiper", {
     clickable: true,
   },
 });
+
+function loader() {
+  document.querySelector(".loader-container").classList.add("fade-out");
+}
+
+function fadeOut() {
+  setInterval(loader, 2500);
+}
+
+window.onload = fadeOut();
